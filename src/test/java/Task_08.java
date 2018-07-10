4a4a35b64086dc48c0884f224cee7dffb5a70352
Sergey Kopantsev <s.kopantsev@gmail.com>
2018-07-10 22:01:09 +0700

    Правки по Заданию 8

--- src/test/java/Task_08.java
+++ src/test/java/Task_08.java
@@ -1,27 +1,19 @@
 import org.junit.Assert;
 import org.openqa.selenium.By;
-import org.openqa.selenium.NoSuchElementException;
-import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.testng.annotations.Test;
 
-public class Task_08 extends Chrome {
+import java.util.List;
 
-    private boolean isElementPresent(WebDriver driver, By locator) {
-        try {
-            driver.findElement(locator);
-            return true;
-        } catch (NoSuchElementException ex) {
-            return false;
-        }
-    }
+public class Task_08 extends Chrome {
 
     @Test
     public void Execute() {
         driver.navigate().to("http://localhost/litecart/");
 
-        int numberOfItems = driver.findElements(By.xpath("//*[@class='product column shadow hover-light']")).size();
-        for (int i = 1; i <= numberOfItems; i++) {
-            Assert.assertTrue(isElementPresent(driver, By.xpath("(//*[@class='product column shadow hover-light'])[" + i + "]/a/div/div[contains (@class, 'sticker')]")));
+        List<WebElement> products = driver.findElements(By.className("product"));
+        for (int i = 0; i < products.size(); i++) {
+            Assert.assertNotNull(products.get(i).findElement(By.xpath("./a/div/div[contains (@class, 'sticker')]")));
         }
         driver.close();
     }
